05795d9a6ae0c542a7c9a2b70558556de15d6a08
Rendra iwan <145772201+RendraIwan@users.noreply.github.com>
2023-09-27 16:28:00 +0700

    Add files via upload

---
+++ Modul Praktikum PBO 2/Modul Praktikum PBO 2/processorBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Perlaptopan
+{
+    internal class processorBase
+    {
+        public override string ToString()
+        {
+            return merk;
+            return tipe;
+        }
+    }
+}
